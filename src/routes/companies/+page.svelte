3843fa634247d6fc0940ed35b71922962b1d8d37
hulk-higakijin <hm385.chejptks@gmail.com>
2023-08-24 16:43:34 +0900

    feat: get companies

---
+++ src/routes/companies/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let data;
+	export let companies = data.companies;
+</script>
+
+<div>
+	<h1>Companies</h1>
+	<ul>
+		{#each companies as company}
+			<li>
+				<a href="/companies/{company.id}">{company.name}</a>
+			</li>
+		{/each}
+	</ul>
+</div>
